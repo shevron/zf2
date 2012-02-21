654667753d80141d3d60722bf1752ac4ac26a7c9
Shahar Evron <shahar.e@zend.com>
2012-02-21 17:56:24 +0200

    adding some basic client tests

--- tests/Zend/Http/ClientTest.php
+++ tests/Zend/Http/ClientTest.php
@@ -5,16 +5,53 @@
  */
 namespace ZendTest\Http;
 
-use Zend\Http\Client;
+use Zend\Http\Client,
+    Zend\Http\Transport\Test as TestTransport,
+    Zend\Http\Transport\Socket as SocketTransport;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
-    /*
-    public function testClientRetrievesUppercaseHttpMethodFromRequestObject()
+    /**
+     * HTTP client object
+     *
+     * @var Zend\Http\Client
+     */
+    protected $client = null;
+
+    /**
+     * HTTP transport object
+     *
+     * @var Zend\Http\Transport\Transport
+     */
+    protected $transport = null;
+
+    public function setUp()
     {
-        $client = new Client;
-        $client->setMethod('post');
-        $this->assertEquals(Client::ENC_URLENCODED, $client->getEncType());
+        $this->transport = new TestTransport();
+        $this->client = new Client();
+        $this->client->setTransport($this->transport);
     }
-    */
+
+    public function tearDown()
+    {
+        unset($this->client);
+        unset($this->transport);
+    }
+
+    public function testDefaultTransportIsSocket()
+    {
+        $client = new Client();
+        $this->assertTrue($client->getTransport() instanceof SocketTransport);
+    }
+
+    public function testSetGetTransport()
+    {
+        $this->client->setTransport($this->transport);
+        $this->assertSame($this->transport, $this->client->getTransport());
+    }
+
+    public function testRedirectCountIsZero()
+    {
+        $this->assertEquals(0, $this->client->getRedirectionsCount());
+    }
 }
