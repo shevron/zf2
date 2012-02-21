f73ba3a3f21fe3fd05a6753c7eec07eef09b2e85
Shahar Evron <shahar.e@zend.com>
2012-02-21 18:17:02 +0200

    adding test to ensure redirect counting works

--- tests/Zend/Http/ClientTest.php
+++ tests/Zend/Http/ClientTest.php
@@ -7,7 +7,9 @@ namespace ZendTest\Http;
 
 use Zend\Http\Client,
     Zend\Http\Transport\Test as TestTransport,
-    Zend\Http\Transport\Socket as SocketTransport;
+    Zend\Http\Transport\Socket as SocketTransport,
+    Zend\Http\Request,
+    Zend\Http\Response;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
@@ -21,7 +23,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     /**
      * HTTP transport object
      *
-     * @var Zend\Http\Transport\Transport
+     * @var Zend\Http\Transport\Test
      */
     protected $transport = null;
 
@@ -53,5 +55,20 @@ class ClientTest extends \PHPUnit_Framework_TestCase
     public function testRedirectCountIsZero()
     {
         $this->assertEquals(0, $this->client->getRedirectionsCount());
+    }
+
+    public function testRedirectionCountIncremented()
+    {
+        $respQueue = $this->transport->getResponseQueue();
+        $respQueue->enqueue(Response::fromString("HTTP/1.1 301 Moved Permanently\r\nLocation: /otherUrl\r\n\r\n"));
+        $respQueue->enqueue(Response::fromString("HTTP/1.1 301 Moved Permanently\r\nLocation: /oneMoreLocation\r\n\r\n"));
+        $respQueue->enqueue(Response::fromString("HTTP/1.1 200 Ok\r\nContent-length: 0\r\n\r\n"));
+
+        $request = new Request();
+        $request->setUri("http://www.example.com/");
+        $resp = $this->client->send($request);
+
+        $this->assertEquals(200, $resp->getStatusCode());
+        $this->assertEquals(2, $this->client->getRedirectionsCount());
     }
 }
